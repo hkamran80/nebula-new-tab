c36be8d4b67d53231504da2a9ce341f2020a393c
H. Kamran <hkamran@unisontech.org>
2021-10-12 11:02:53 -0700

    Update Nebula site

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     mode: "jit",
-    purge: ["./src/*.{html}"],
+    purge: ["./docs/*.{html,css}"],
     theme: {
         fontFamily: {
             sans: ["Quicksand", ...defaultTheme.fontFamily.sans],
